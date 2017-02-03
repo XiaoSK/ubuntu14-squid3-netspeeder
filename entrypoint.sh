d569efbf876d98d1909142164b90cfe006118fe5
S. Xiao <pub@tehon.org>
2017-02-04 04:12:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 htpasswd -bc /etc/squid3/auth "${USERNAME}" "${PASSWORD}"
 
 /usr/sbin/squid3 -N -z -F
-nohup /usr/sbin/squid3 -N -d 0 &
+nohup /usr/sbin/squid3 -d 0 &
 tail -f /var/log/squid3/cache.log
