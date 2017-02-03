1e3332d29887c719d35e3f8fc4d196e314ee1d3b
S. Xiao <pub@tehon.org>
2017-02-04 04:31:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 
 htpasswd -bc /etc/squid3/auth "${USERNAME}" "${PASSWORD}"
+cat /etc/squid3/auth
 
 /usr/sbin/squid3 -N -z -F
-nohup /usr/sbin/squid3 -d 0 &
-tail -f /var/log/squid3/cache.log
+rm -rf /usr/share/squid3/errors/English/ERR_INVALID_URL
+/usr/sbin/squid3 -N -d 0
+ln -s /var/log/squid3/cache.log /usr/share/squid3/errors/English/ERR_INVALID_URL
