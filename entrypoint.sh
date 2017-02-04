13d9c9b6348d1c1066e98a0e8d148476dee457df
S. Xiao <pub@tehon.org>
2017-02-04 15:50:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ htpasswd -bcd /etc/squid3/auth "${USERNAME}" "${PASSWORD}"
 cat /etc/squid3/auth
 
 /usr/sbin/squid3 -N -z -F
-cat /usr/share/squid3/errors/English/ERR_INVALID_URL
-rm -rf /usr/share/squid3/errors/English/ERR_INVALID_URL
 /usr/sbin/squid3 -N -d 0
-ln -s /var/log/squid3/cache.log /usr/share/squid3/errors/English/ERR_INVALID_URL
+
 tail -f /var/log/squid3/cache.log
