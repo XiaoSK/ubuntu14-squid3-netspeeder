30d0aeb241c7cd80d1eda02290ea197ad45fe65c
S. Xiao <pub@tehon.org>
2017-02-08 20:35:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 
 # Please Set Squid Proxy Username and Password in ENV
 sed -i "s/USER_LIST_PLACEHOLDER/${USERNAME}/g" /etc/squid3/squid.conf
-cat /etc/squid3/squid.conf
 htpasswd -bcd /etc/squid3/auth "${USERNAME}" "${PASSWORD}"
 
 /usr/sbin/squid3 -N -z -F
